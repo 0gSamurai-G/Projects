6c8ab31ad4467c96a8108be398b56c274afa125b
0gSamurai-G <srdeshpande1122@gmail.com>
2024-07-21 23:59:37 +0530

    new

---
+++ if.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the amount=\n";
+    cin>>a;
+
+    if(a>5000){
+        cout<<"Expensive\n";
+    } 
+    else{
+        cout<<"Cheap\n";
+    }
+    return 0;
+    
+
+}
